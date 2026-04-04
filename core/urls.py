dc5204568baa03154a950039ce2c65169618e9ab
amir <amirmadani901@gmail.com>
2026-04-04 18:21:28 +0330

    fix some errors

--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("" , include("accounts.urls")),
     path("" , include("todo.urls")),
     path ('api-auth/' , include("rest_framework.urls"))
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- todo/urls.py
+++ todo/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("complete-task/<int:pk>/" , CompleteTaskView.as_view() , name = "complete_task"),
     path("undone-task/<int:pk>/", UndoneTaskView.as_view() ,name = "undone_task"),
     path('api/v1/' , include('todo.api.v1.urls'))
+    
 ]
